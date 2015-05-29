751d043e029a44b7fd4932637db9afcddd6dac92
Jim <jim.brooke@cern.ch>
2015-05-29 15:18:47 +0200

    switch to 750pre4

--- L1Trigger/L1TNtuples/python/RelValInputFiles.py
+++ L1Trigger/L1TNtuples/python/RelValInputFiles.py
@@ -11,5 +11,7 @@ def RelValInputFile_DIGI():
 
 # RAW (need to run RawToDigi to use this)
 def RelValInputFile_RAW():
-    return '/store/relval/CMSSW_7_5_0_pre1/RelValProdTTbar_13/GEN-SIM-RAW/MCRUN2_74_V7-v1/00000/0CEB1526-6CE3-E411-82B6-00261894386C.root'
+    return '/store/relval/CMSSW_7_5_0_pre4/RelValProdTTbar_13/GEN-SIM-RAW/MCRUN2_75_V1-v1/00000/1CFADAF5-E1F5-E411-A406-0025905A60D6.root'
+
+#'/store/relval/CMSSW_7_5_0_pre1/RelValProdTTbar_13/GEN-SIM-RAW/MCRUN2_74_V7-v1/00000/0CEB1526-6CE3-E411-82B6-00261894386C.root'
 
