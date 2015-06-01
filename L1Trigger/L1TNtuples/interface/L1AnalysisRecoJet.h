0464cb80175c223ed1c74a2e6a75607985dc9722
Jim <jim.brooke@cern.ch>
2015-06-01 18:38:29 +0200

    reco jet update

--- L1Trigger/L1TNtuples/interface/L1AnalysisRecoJet.h
+++ L1Trigger/L1TNtuples/interface/L1AnalysisRecoJet.h
@@ -8,6 +8,7 @@
 // Original code : L1Trigger/L1TNtuples/L1RecoJetNtupleProducer - Jim Brooke
 //-------------------------------------------------------------------------------
 
+#include "JetMETCorrections/JetCorrector/interface/JetCorrector.h"
 #include "DataFormats/JetReco/interface/CaloJetCollection.h"
 #include "DataFormats/JetReco/interface/JetID.h"
 #include "L1AnalysisRecoJetDataFormat.h"
@@ -15,7 +16,6 @@
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/EventSetup.h"
 
-class JetCorrector;
 
 namespace L1Analysis
 {
@@ -29,8 +29,8 @@ namespace L1Analysis
     void SetCaloJet(const edm::Event& event,
 		    const edm::EventSetup& setup,
 		    const edm::Handle<reco::CaloJetCollection> caloJets, 
-		    edm::Handle<edm::ValueMap<reco::JetID> > jetsID,
-		    const JetCorrector* jetCorrector,
+		    //edm::Handle<edm::ValueMap<reco::JetID> > jetsID,
+		    //edm::Handle<reco::JetCorrector> jetCorrector,
 		    unsigned maxJet);
     L1AnalysisRecoJetDataFormat * getData() {return &recoJet_;}
     void Reset() {recoJet_.Reset();}
--- L1Trigger/L1TNtuples/python/l1RecoTreeProducer_cfi.py
+++ L1Trigger/L1TNtuples/python/l1RecoTreeProducer_cfi.py
@@ -2,8 +2,10 @@ import FWCore.ParameterSet.Config as cms
 
 l1RecoTreeProducer = cms.EDAnalyzer("L1RecoTreeProducer",
 
-  jetTag                  = cms.untracked.InputTag("ak5CaloJets"),
-  jetIdTag                = cms.untracked.InputTag("ak5JetID"),
+  jetTag                  = cms.untracked.InputTag("ak4CaloJets"),
+  jetIdTag                = cms.untracked.InputTag(""),
+  jetCorrToken            = cms.untracked.InputTag(""),
+
   metTag                  = cms.untracked.InputTag("met"),
   ebRecHitsTag            = cms.untracked.InputTag("ecalRecHit:EcalRecHitsEB"),
   eeRecHitsTag            = cms.untracked.InputTag("ecalRecHit:EcalRecHitsEE"),
@@ -13,8 +15,7 @@ l1RecoTreeProducer = cms.EDAnalyzer("L1RecoTreeProducer",
   basicClustersEndcapTag  = cms.untracked.InputTag("multi5x5BasicClusters:multi5x5EndcapBasicClusters"),
   verticesTag             = cms.untracked.InputTag("offlinePrimaryVertices"),
   tracksTag               = cms.untracked.InputTag("generalTracks"),
-  jetCorrectorServiceName = cms.untracked.string("ak5CaloL1FastL2L3"),
-  #jetCorrectorServiceName = cms.untracked.string("ak5CaloL1L2L3Residual"),
+#  jetCorrectorServiceName = cms.untracked.string(""),
 
   maxCl  = cms.uint32(20),
   maxVtx = cms.uint32(10),
