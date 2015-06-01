0d6da67d26a6b4e4e953adc6d54c5fa9efb68ca6
Jim <jim.brooke@cern.ch>
2015-06-01 18:49:03 +0200

    add placeholder for standard AOD workflow

---
+++ L1Trigger/L1TNtuples/python/L1NtupleAOD_cff.py
@@ -0,0 +1,11 @@
+import FWCore.ParameterSet.Config as cms
+
+from L1Trigger.L1TNtuples.l1ExtraTreeProducer_cfi import *
+from L1Trigger.L1TNtuples.l1MenuTreeProducer_cfi import *
+from L1Trigger.L1TNtuples.l1RecoJetTreeProducer_cfi import *
+
+L1NtupleAOD = cms.Sequence(
+  l1ExtraTreeProducer
+  +l1MenuTreeProducer
+  +l1RecoJetTreeProducer
+)
