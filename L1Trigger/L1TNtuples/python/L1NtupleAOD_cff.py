d90d58e0e7df194be0b8ebc19c9312a8d9e10ba2
Jim <jim.brooke@cern.ch>
2015-06-02 15:20:54 +0200

    add customisation

--- L1Trigger/L1TNtuples/python/L1NtupleAOD_cff.py
+++ L1Trigger/L1TNtuples/python/L1NtupleAOD_cff.py
@@ -2,10 +2,13 @@ import FWCore.ParameterSet.Config as cms
 
 from L1Trigger.L1TNtuples.l1ExtraTreeProducer_cfi import *
 from L1Trigger.L1TNtuples.l1MenuTreeProducer_cfi import *
-from L1Trigger.L1TNtuples.l1RecoJetTreeProducer_cfi import *
+from L1Trigger.L1TNtuples.l1JetRecoTreeProducer_cfi import *
+from L1Trigger.L1TNtuples.l1MuonRecoTreeProducer_cfi import *
 
 L1NtupleAOD = cms.Sequence(
   l1ExtraTreeProducer
   +l1MenuTreeProducer
-  +l1RecoJetTreeProducer
+  +l1JetRecoTreeProducer
+  #+l1MuonRecoTreeProducer
 )
+
