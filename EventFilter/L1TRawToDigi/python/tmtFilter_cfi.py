065b9a6531b0bfd3098181e9795316be4cf2e5ec
Jim <jim.brooke@cern.ch>
2015-11-03 13:07:28 +0100

    bugfix filter name

--- EventFilter/L1TRawToDigi/python/tmtFilter_cfi.py
+++ EventFilter/L1TRawToDigi/python/tmtFilter_cfi.py
@@ -1,7 +1,7 @@
 
 import FWCore.ParameterSet.Config as cms
 
-tmtEventFilter = cms.EDFilter(
+tmtFilter = cms.EDFilter(
     "TMTFilter",
     inputTag  = cms.InputTag("rawDataCollector"),
     mpList    = cms.untracked.vint32(0,1,2,3,4,5,6,7,8)
