fe1d8cf362011f189ef8411259e46012795d5f9a
Aaron <aaron.bundock@cern.ch>
2015-11-09 20:16:32 +0100

    Fixed PU sub.

--- L1Trigger/L1TCalorimeter/src/firmware/Stage2Layer2JetAlgorithmFirmwareImp1.cc
+++ L1Trigger/L1TCalorimeter/src/firmware/Stage2Layer2JetAlgorithmFirmwareImp1.cc
@@ -66,7 +66,7 @@ void l1t::Stage2Layer2JetAlgorithmFirmwareImp1::processEvent(const std::vector<l
 void l1t::Stage2Layer2JetAlgorithmFirmwareImp1::create(const std::vector<l1t::CaloTower> & towers, std::vector<l1t::Jet> & jets, 
 						       std::vector<l1t::Jet> & alljets, std::string PUSubMethod) {
   
-  int etaMax=40, etaMin=1, phiMax=72, phiMin=1;
+  int etaMax=36, etaMin=1, phiMax=72, phiMin=1;
   
   // etaSide=1 is positive eta, etaSide=-1 is negative eta
   for (int etaSide=1; etaSide>=-1; etaSide-=2) {
@@ -318,20 +318,21 @@ int l1t::Stage2Layer2JetAlgorithmFirmwareImp1::chunkyDonutPUEstimate(int jetEta,
       int towPhi = iphi;
       while ( towPhi > phiMax ) towPhi -= phiMax;
       while ( towPhi < phiMin ) towPhi += phiMax;
-
+      
       if (ietaUp>=etaMin && ietaUp<=etaMax) {    
-	const CaloTower& towEtaUp = CaloTools::getTower(towers, ietaUp, towPhi);
-	int towEt = towEtaUp.hwPt();
-	ring[2] += towEt;
+    	const CaloTower& towEtaUp = CaloTools::getTower(towers, ietaUp, towPhi);
+    	int towEt = towEtaUp.hwPt();
+    	ring[2] += towEt;
       }
       
       if (ietaDown>=etaMin && ietaDown<=etaMax) {
-	const CaloTower& towEtaDown = CaloTools::getTower(towers, ietaDown, towPhi);
-	int towEt = towEtaDown.hwPt();
-	ring[3] += towEt;
+    	const CaloTower& towEtaDown = CaloTools::getTower(towers, ietaDown, towPhi);
+    	int towEt = towEtaDown.hwPt();
+    	ring[3] += towEt;
       }
       
-    } 
+    }
+ 
     
   }
   
