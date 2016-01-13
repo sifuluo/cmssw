9dbe2ff784b44092a67e706fbb849de985239edc
Michael Mulhearn <mulhearn@cern.ch>
2016-01-13 02:37:58 +0100

    New Condidtions for L1TGlobal

---
+++ CondFormats/DataRecord/interface/L1TUtmCutValueRcd.h
@@ -0,0 +1,6 @@
+// auto-generated file by import_utm.pl 
+#ifndef CondFormatsDataRecord_L1TUtmCutValueRcd_h
+#define CondFormatsDataRecord_L1TUtmCutValueRcd_h
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+class L1TUtmCutValueRcd : public edm::eventsetup::EventSetupRecordImplementation<L1TUtmCutValueRcd> {};
+#endif
---
+++ CondFormats/DataRecord/src/L1TUtmAlgorithmRcd.cc
@@ -0,0 +1,4 @@
+// auto-generated file by import_utm.pl 
+#include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
+#include "CondFormats/DataRecord/interface/L1TUtmAlgorithmRcd.h"
+EVENTSETUP_RECORD_REG(L1TUtmAlgorithmRcd);
