a039edddfff625d7ab5d7c6f3e11a332f48fef05
Jim <jim.brooke@cern.ch>
2016-01-23 15:05:28 +0100

    use CaloTools to convert to GT units

--- L1Trigger/L1TCalorimeter/src/firmware/Stage2Layer2DemuxJetAlgoFirmwareImp1.cc
+++ L1Trigger/L1TCalorimeter/src/firmware/Stage2Layer2DemuxJetAlgoFirmwareImp1.cc
@@ -43,60 +43,27 @@ l1t::Stage2Layer2DemuxJetAlgoFirmwareImp1::~Stage2Layer2DemuxJetAlgoFirmwareImp1
 void l1t::Stage2Layer2DemuxJetAlgoFirmwareImp1::processEvent(const std::vector<l1t::Jet> & inputJets,
                                                              std::vector<l1t::Jet> & outputJets) {
 
-  // Set the output jets to the input jets
   outputJets = inputJets;
 
   // Sort the jets by pT
   std::vector<l1t::Jet>::iterator start(outputJets.begin());
   std::vector<l1t::Jet>::iterator end(outputJets.end());
 
+  //  for (auto& jet: outputJets){
+  //    std::cout << "MP : " << jet.hwPt() << ", " << jet.hwEta() << ", " << jet.hwPhi() << ", " << CaloTools::towerEta(jet.hwEta()) << ", " << CaloTools::towerPhi(jet.hwEta(),jet.hwPhi()) << std::endl;
+  //  }
+
   BitonicSort< l1t::Jet >(down,start,end);
 
 
   // convert eta to GT coordinates
-  vector<pair<int,double> > etaGT;
-  for(int i=0;i<115;i++)
-    etaGT.push_back( make_pair(i,i*(0.087/2.)) );
-
-  vector<pair<int,double> > phiGT;
-  for(int i=0;i<145;i++)
-    phiGT.push_back( make_pair(i,i*(M_PI/72.)) );
-  phiGT[144] = make_pair(0,2*M_PI); //2pi = 0
-
-
   for(auto& jet : outputJets){
 
-    double eta = CaloTools::towerEta(jet.hwEta());
-    double phi = CaloTools::towerEta(jet.hwPhi());
- 
-    double minDistance = 99999.;
-    pair<int, double> closestPoint = make_pair(0,0.);
-    
-    for(const auto& p : etaGT){
-      double distance = abs(abs(eta) - p.second);
-      if(distance < minDistance){
-	closestPoint = p;
-	minDistance = distance;
-      }
-    }
+    int gtEta = CaloTools::gtEta(jet.hwEta());
+    int gtPhi = CaloTools::gtPhi(jet.hwPhi());
 
-    int hwEta_GT = (eta>0) ? closestPoint.first : - closestPoint.first;
-
-    minDistance = 99999.;
-    closestPoint = make_pair(0,0.);
-    
-    for(const auto& p : phiGT){
-      double distance = abs(phi - p.second);
-      if(distance < minDistance){
-	closestPoint = p;
-	minDistance = distance;
-      }
-    }
-
-    int hwPhi_GT = closestPoint.first;
-
-    jet.setHwEta(hwEta_GT);
-    jet.setHwPhi(hwPhi_GT);
+    jet.setHwEta(gtEta);
+    jet.setHwPhi(gtPhi);
 
   }
 
