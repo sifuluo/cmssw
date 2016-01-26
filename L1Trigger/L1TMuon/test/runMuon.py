a45712b047e4b274049bf98c20ed34004024fc83
Thomas Reis <thomas.reis@cern.ch>
2016-01-26 13:45:54 +0100

    Changes to run reEmul.py and runMuon.py

--- L1Trigger/L1TMuon/test/runMuon.py
+++ L1Trigger/L1TMuon/test/runMuon.py
@@ -4,6 +4,9 @@ import os
 import sys
 import commands
 
+from Configuration.StandardSequences.Eras import eras
+process = cms.Process("L1TMuonEmulation", eras.Run2_2016)
+
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 
 process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(50)
@@ -24,18 +27,18 @@ from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
 process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc', '')
 
 ####Event Setup Producers
-process.load('L1Trigger.L1TMuonBarrel.fakeMuonBarrelParams_cfi')
-process.load('L1Trigger.L1TMuonOverlap.fakeMuonOverlapParams_cfi')
-process.load('L1Trigger.L1TMuonEndCap.fakeMuonEndCapParams_cfi')
-process.load('L1Trigger.L1TMuon.fakeMuonGlobalParams_cfi')
-
+process.load('L1Trigger.L1TMuon.hackConditions_cff')
 
 #### Emulators
-process.load('L1Trigger.L1TMuonBarrel.simMuonBarrelDigis_cfi')
-process.load('L1Trigger.L1TMuonOverlap.simMuonOverlapDigis_cfi')
-process.load('L1Trigger.L1TMuonEndCap.simMuonEndCapDigis_cfi')
+process.load('L1Trigger.L1TCalorimeter.simCaloStage2Layer1Digis_cfi')
+process.simCaloStage2Layer1Digis.ecalToken = cms.InputTag("simEcalTriggerPrimitiveDigis")
+process.simCaloStage2Layer1Digis.hcalToken = cms.InputTag("simHcalTriggerPrimitiveDigis")
+process.load('L1Trigger.L1TMuonBarrel.simTwinMuxDigis_cfi')
+process.load('L1Trigger.L1TMuonBarrel.simBmtfDigis_cfi')
+process.load('L1Trigger.L1TMuonEndCap.simEmtfDigis_cfi')
+process.load('L1Trigger.L1TMuonOverlap.simOmtfDigis_cfi')
+process.load('L1Trigger.L1TMuon.simGmtCaloSumDigis_cfi')
 process.load('L1Trigger.L1TMuon.simMuonDigis_cfi')
-process.load('L1Trigger.L1TCalorimeter.caloStage2Layer1Digis_cfi')
 
 process.dumpED = cms.EDAnalyzer("EventContentAnalyzer")
 process.dumpES = cms.EDAnalyzer("PrintEventSetupContent")
@@ -45,14 +48,10 @@ process.dumpES = cms.EDAnalyzer("PrintEventSetupContent")
 #process.l1tSummary.tauToken  = cms.InputTag("simCaloStage2Digis");
 #process.l1tSummary.jetToken  = cms.InputTag("simCaloStage2Digis");
 #process.l1tSummary.sumToken  = cms.InputTag("simCaloStage2Digis");
-#process.l1tSummary.muonToken = cms.InputTag("simGmtDigis","");
-##process.l1tSummary.muonToken = cms.InputTag("simGmtDigis","imdMuonsBMTF");
+#process.l1tSummary.muonToken = cms.InputTag("simGmtStage2Digis","");
+##process.l1tSummary.muonToken = cms.InputTag("simGmtStage2Digis","imdMuonsBMTF");
 
-process.load('L1Trigger.L1TCalorimeter.caloStage2Layer1Digis_cfi')
-process.caloStage2Layer1Digis.ecalToken = cms.InputTag("simEcalTriggerPrimitiveDigis")
-process.caloStage2Layer1Digis.hcalToken = cms.InputTag("simHcalTriggerPrimitiveDigis")
-
-process.L1TMuonSeq = cms.Sequence(   process.caloStage2Layer1Digis
+process.L1TMuonSeq = cms.Sequence(   process.simCaloStage2Layer1Digis
                                    + process.simTwinMuxDigis
                                    + process.simBmtfDigis 
                                    + process.simEmtfDigis 
@@ -67,7 +66,7 @@ process.L1TMuonSeq = cms.Sequence(   process.caloStage2Layer1Digis
 process.L1TMuonPath = cms.Path(process.L1TMuonSeq)
 
 process.out = cms.OutputModule("PoolOutputModule", 
-   fileName = cms.untracked.string("l1tbmtf_superprimitives1.root")
+   fileName = cms.untracked.string("l1tmuon.root")
 )
 
 process.output_step = cms.EndPath(process.out)
