cb609b33b5017f6d78df0bedff821a019af4cfd1
Vladimir Rekovic <vladimir.rekovic@cern.ch>
2016-04-14 12:52:40 +0200

    Adjust L1 ReEmul custom configuration so that in both DATA and MC all use simCscTriggerPrimitveDigis

--- L1Trigger/Configuration/python/customiseReEmul.py
+++ L1Trigger/Configuration/python/customiseReEmul.py
@@ -69,8 +69,6 @@ def L1TReEmulFromRAW(process):
         process.simOmtfDigis.srcRPC                = cms.InputTag('muonRPCDigis')
         process.simBmtfDigis.DTDigi_Source         = cms.InputTag("simTwinMuxDigis")
         process.simBmtfDigis.DTDigi_Theta_Source   = cms.InputTag("dttfDigis")
-        process.simEmtfDigis.CSCInput              = cms.InputTag("csctfDigis")
-        process.simOmtfDigis.srcCSC                = cms.InputTag("csctfDigis")
         process.simCaloStage2Layer1Digis.ecalToken = cms.InputTag("ecalDigis:EcalTriggerPrimitives")
         process.L1TReEmulPath = cms.Path(process.L1TReEmul)    
         process.schedule.append(process.L1TReEmulPath)
@@ -91,7 +89,4 @@ def L1TReEmulFromRAW(process):
 
 def L1TReEmulMCFromRAW(process):
     L1TReEmulFromRAW(process)
-    if eras.stage2L1Trigger.isChosen():
-        process.simEmtfDigis.CSCInput           = cms.InputTag('simCscTriggerPrimitiveDigis','MPCSORTED')
-        process.simOmtfDigis.srcCSC             = cms.InputTag('simCscTriggerPrimitiveDigis','MPCSORTED')
     return process
