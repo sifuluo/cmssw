2409c33b3dd4cbda6d8d3434a46708031f791906
Vladimir Rekovic <vladimir.rekovic@cern.ch>
2016-04-21 21:30:45 +0200

    Calo unapcker to use FED ID 1366 only, as in Calo packer

--- EventFilter/L1TRawToDigi/python/caloStage2Digis_cfi.py
+++ EventFilter/L1TRawToDigi/python/caloStage2Digis_cfi.py
@@ -3,5 +3,5 @@ import FWCore.ParameterSet.Config as cms
 caloStage2Digis = cms.EDProducer(
     "L1TRawToDigi",
     Setup           = cms.string("stage2::CaloSetup"),
-    FedIds          = cms.vint32( 1360, 1366 ),
+    FedIds          = cms.vint32(1366),
 )
