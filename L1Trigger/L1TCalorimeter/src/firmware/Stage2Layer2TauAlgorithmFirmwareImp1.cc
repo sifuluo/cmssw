a512b3711ef387c18d695e357532f94a8d74b59d
Olivier Davignon <davignon@cern.ch>
2016-04-25 15:12:30 +0200

    Enable 2 LUTs for tau isolation

--- L1Trigger/L1TCalorimeter/src/firmware/Stage2Layer2TauAlgorithmFirmwareImp1.cc
+++ L1Trigger/L1TCalorimeter/src/firmware/Stage2Layer2TauAlgorithmFirmwareImp1.cc
@@ -135,6 +135,8 @@ void l1t::Stage2Layer2TauAlgorithmFirmwareImp1::merging(const std::vector<l1t::C
                 if (hwIsoEnergy < 0) hwIsoEnergy = 0; // just in case the cluster is outside the window? should be very rare
                 
                 isolBit = (hwIsoEnergy <= (params_->tauIsolationLUT()->data(LUTaddress)) ? 1 : 0);
+		int isolBit2 = (hwIsoEnergy <= (params_->tauIsolationLUT2()->data(LUTaddress)) ? 1 : 0);
+		isolBit += (isolBit2 << 1);
                 tau.setHwIso(isolBit);
 
                 // development vars
@@ -418,6 +420,8 @@ void l1t::Stage2Layer2TauAlgorithmFirmwareImp1::merging(const std::vector<l1t::C
                 if (hwIsoEnergy < 0) hwIsoEnergy = 0; // just in case the cluster is outside the window? should be very rare
 
                 isolBit = (hwIsoEnergy <= (params_->tauIsolationLUT()->data(LUTaddress)) ? 1 : 0);
+		int isolBit2 = (hwIsoEnergy <= (params_->tauIsolationLUT2()->data(LUTaddress)) ? 1 : 0);
+		isolBit += (isolBit2 << 1);
                 tau.setHwIso(isolBit);
 
                 // development vars
