4a5709cb0b00721a0016d5be8c106cbc17662ae0
Aaron <aaron.bundock@cern.ch>
2018-09-12 06:24:03 +0200

    Update HI dev, add HI caloParams

--- L1Trigger/L1TCalorimeter/interface/CaloParamsHelper.h
+++ L1Trigger/L1TCalorimeter/interface/CaloParamsHelper.h
@@ -56,7 +56,8 @@ namespace l1t {
 	   egBypassHoEFlag=44,
 	   etSumCentralityLower=45,
 	   etSumCentralityUpper=46,
-	   NUM_CALOPARAMNODES=47
+           jetPUSUseChunkySandwichFlag=47,
+	   NUM_CALOPARAMNODES=48
     };
 
     CaloParamsHelper() { pnode_.resize(NUM_CALOPARAMNODES); }
@@ -321,6 +322,7 @@ namespace l1t {
     }
 
     unsigned jetBypassPUS() const { return pnode_[jetBypassPUSFlag].uparams_[0]; }
+    unsigned jetPUSUseChunkySandwich() const { return pnode_[jetPUSUseChunkySandwichFlag].uparams_[0]; }
 
     std::string jetPUSType() const { return pnode_[jetPUS].type_; }
     std::vector<double> jetPUSParams() { return pnode_[jetPUS].dparams_; }
@@ -348,6 +350,10 @@ namespace l1t {
     void setJetBypassPUS(unsigned flag) { 
       pnode_[jetBypassPUSFlag].uparams_.resize(1);
       pnode_[jetBypassPUSFlag].uparams_[0] = flag; 
+    }
+    void setJetPUSUseChunkySandwich(unsigned flag) { 
+      pnode_[jetPUSUseChunkySandwichFlag].uparams_.resize(1);
+      pnode_[jetPUSUseChunkySandwichFlag].uparams_[0] = flag; 
     }
     
     // sums
